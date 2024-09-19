ceb9eb19b6174d3881da4fbfb27d03c3afd99f5a
akshay agney <akshayagneyy41@gmail.com>
2024-09-19 21:29:52 +0530

    first react project for deployment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  
+  return (
+    <>
+    <div className="bg-zinc-900 text-white p-4 h-screen w-screen flex flex-col justify-center items-center">
+   <h1 className="text-4xl font-bold mb-2 ">Hello World,👋</h1>
+   <h1 className="text-4xl font-bold mb-2">I am Akshay Agney</h1>
+   <p className="text-lg mb-4">This is a React App</p>
+   <a href="https://github.com/akshay-agney" className="text-blue-500 hover:text-blue-600"><i className="ri-github-fill text-4xl"></i></a>
+   </div>
+   </>
+  )
+}
+
+export default App
